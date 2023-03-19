adb5f1881097681a8580dc17d0ff9bc0e9b67e8d
kjozwicki <100798539+kjozwicki@users.noreply.github.com>
2023-03-19 22:04:55 +0100

    etap 1 struktura

---
+++ Kugle/Kugle/Data/BallData.cs
@@ -0,0 +1,63 @@
+namespace Data
+{
+    internal class BallData
+    {
+        private int _xPosition;
+        private int _yPosition;
+        private int _xSpeed;
+        private int _ySpeed;
+        private bool _moving = true;
+
+        private const int FluentMoveTime = 8;
+
+        public BallData(int xPosition, int yPosition, int radius, int weight, int xSpeed, int ySpeed)
+        {
+            _xPosition = xPosition;
+            _yPosition = yPosition;
+            _xSpeed = xSpeed;
+            _ySpeed = ySpeed;
+            Radius = radius;
+            Weight = weight;
+        }
+
+        public int XPosition
+        {
+            get => _xPosition;
+            internal set
+            {
+                _xPosition = value;
+            }
+        }
+
+        public int YPosition
+        {
+            get => _yPosition;
+            internal set
+            {
+                _yPosition = value;
+            }
+        }
+
+        public int Weight { get; }
+
+        public int Radius { get; }
+
+        public int XSpeed
+        {
+            get => _xSpeed;
+            set
+            {
+                _xSpeed = value;
+            }
+        }
+
+        public int YSpeed
+        {
+            get => _ySpeed;
+            set
+            {
+                _ySpeed = value;
+            }
+        }
+    }
+}
